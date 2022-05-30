c3b9716e1105850c9d7f86317ae5f89725978b93
sheriff-gaye <sheriffgaye5@gmail.com>
2022-05-30 23:36:04 +0000

    python

--- 0x00-python-hello_world/list.h
+++ 0x00-python-hello_world/list.h
@@ -2,6 +2,8 @@
 #define LISTS_H
 
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 /**
  * struct listint_s - singly linked list
@@ -11,6 +13,7 @@
  * Description: singly linked list node structure
  * for Holberton project
  */
+
 typedef struct listint_s
 {
 	int n;
@@ -21,5 +24,7 @@ size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
+int check_cycle(listint_t *list);
 
-#endif /* LISTS_H */
+#endif
+/* LISTS_H */
