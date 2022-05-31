2e112752ad26c9d87f86bb253c9cc767bd21ecdc
sheriff-gaye <sheriffgaye5@gmail.com>
2022-05-31 13:29:02 +0000

    python

--- 0x01-python-if_else_loops_functions/8-main.py
+++ 0x01-python-if_else_loops_functions/8-main.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
 uppercase = __import__('8-uppercase').uppercase
-
 uppercase("best")
-uppercase("Best School 98 Battery street")
+uppercase("\nBest School 98 Battery street")
