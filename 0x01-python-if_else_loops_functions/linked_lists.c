4d50094adb438fe0ac1e5056bfff3adb35fd3e7e
sheriff-gaye <sheriffgaye5@gmail.com>
2022-05-31 17:47:48 +0000

    python

--- 0x01-python-if_else_loops_functions/linked_lists.c
+++ 0x01-python-if_else_loops_functions/linked_lists.c
@@ -9,15 +9,15 @@
  * @h: pointer to head of list
  * Return: number of nodes
  */
-size_t print_listint(const listint_t * h) {
-  const listint_t * current;
+size_t print_listint(const listint_t *h) {
+  const listint_t *current;
   unsigned int n; /* number of nodes */
 
   current = h;
   n = 0;
   while (current != NULL) {
-    printf("%i\n", current -> n);
-    current = current -> next;
+    printf("%i\n", current->n);
+    current = current->next;
     n++;
   }
 
@@ -30,27 +30,25 @@ size_t print_listint(const listint_t * h) {
  * @n: integer to be included in new node
  * Return: address of the new element or NULL if it fails
  */
-listint_t * add_nodeint_end(listint_t ** head,
-  const int n) {
-  listint_t * new;
-  listint_t * current;
+listint_t *add_nodeint_end(listint_t **head, const int n) {
+  listint_t *new;
+  listint_t *current;
 
-  current = * head;
+  current = *head;
 
   new = malloc(sizeof(listint_t));
   if (new == NULL)
     return (NULL);
 
-  new -> n = n;
-  new -> next = NULL;
+  new->n = n;
+  new->next = NULL;
 
-  if ( * head == NULL)
-    *
-    head = new;
+  if (*head == NULL)
+    *head = new;
   else {
-    while (current -> next != NULL)
-      current = current -> next;
-    current -> next = new;
+    while (current->next != NULL)
+      current = current->next;
+    current->next = new;
   }
 
   return (new);
@@ -61,12 +59,12 @@ listint_t * add_nodeint_end(listint_t ** head,
  * @head: pointer to list to be freed
  * Return: void
  */
-void free_listint(listint_t * head) {
-  listint_t * current;
+void free_listint(listint_t *head) {
+  listint_t *current;
 
   while (head != NULL) {
     current = head;
-    head = head -> next;
+    head = head->next;
     free(current);
   }
 }
