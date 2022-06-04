fab4465244236c9fcdab548c85d06701636a78cd
sheriff-gaye <sheriffgaye5@gmail.com>
2022-06-04 17:45:26 +0000

    data

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = my_string.replace('C','',2)
+    new_string = my_string.replace('C', '', 2)
+    new_string = my_string.replace('c', '', 2)
     return new_string
