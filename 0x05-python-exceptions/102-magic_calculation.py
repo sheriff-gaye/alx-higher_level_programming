984204c2dfe51bdd0ed4c6a10798730108bceea2
sheriff-gaye <sheriffgaye5@gmail.com>
2022-06-20 16:55:06 +0000

    advnc

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,4 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
 def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
@@ -11,4 +14,3 @@ def magic_calculation(a, b):
             result = b + a
             break
     return result
-    
