51720dd14162e9df6cfc73a946ee0ffd9e44eeb8
sheriff-gaye <sheriffgaye5@gmail.com>
2022-06-20 15:35:58 +0000

    try

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,5 +5,5 @@ def safe_print_division(a, b):
     except(ZeroDivisionError, TypeError):
         div = None
     finally:
-        print("Inside result:{}".format(div))
+        print("Inside result: {}".format(div))
     return (div)
