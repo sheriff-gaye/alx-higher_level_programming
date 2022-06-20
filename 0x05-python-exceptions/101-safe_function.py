e23a192aef15f96b9cef010b530700c3ff77012b
sheriff-gaye <sheriffgaye5@gmail.com>
2022-06-20 15:48:11 +0000

    advnc

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -2,7 +2,6 @@
 
 import sys
 
-
 def safe_function(fct, *args):
     try:
         result = fct(*args)
@@ -10,3 +9,4 @@ def safe_function(fct, *args):
     except:
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return (None)
+
