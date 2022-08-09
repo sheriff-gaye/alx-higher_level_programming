2e31310f6e4cca12d7142b5a4af52b026d3ecd6c
sheriff-gaye <sheriffgaye5@gmail.com>
2022-08-09 16:46:43 +0000

    sql

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,8 @@
+-- Displays the average temperature (in Fahrenheit)
+-- by city ordered by descending temperature.
+SELECT `city`,AVG(`value`) AS `avg_temp`
+FROM  `temperatures`
+WHERE `month` = 7 OR `month` = 8
+GROUP BY `city`
+ORDER BY `avg_temp` DESC
+LIMIT 3;
