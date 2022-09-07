862212f692f2d816b5bfe486326330f0204211e5
sheriff-gaye <sheriffgaye5@gmail.com>
2022-09-07 02:58:24 +0000

    js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
-
-const list = require('./100-data').list;
+const list = require('./100-data.js').list;
 console.log(list);
-const newList = list.map((v, i) => v * i);
-console.log(newList);
+console.log(list.map((item, index) => item * index));
